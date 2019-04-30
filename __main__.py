11dc442e88a3354bd20eac99a88b8b0247d3e201
Eddie Chiang <eddiecjc@gmail.com>
2019-04-30 20:54:18 +1200

    Move Dialgue Act classification into the submodule.

--- __main__.py
+++ __main__.py
@@ -34,41 +34,8 @@ def main():
             dialogue_act_classifier = pickle.load(f)
             logger.info('Loaded trained dialogue act classifier.')
     else:
-        # Extract the labeled basic messaging data.
-        #posts = nltk.corpus.nps_chat.xml_posts()[:10000]
-        posts = nltk.corpus.nps_chat.xml_posts()
-        logger.info(f'Loaded {len(posts)} posts from nps_chat corpus.')
-
-        # Construct the training and test data by applying the feature extractor to each post, and create a new classifier.
-        featuresets = [(dac_classifier.dialogue_act_features(post.text), post.get('class'))
-                        for post in posts]
-        size = int(len(featuresets) * 0.1) # 10% to use as Training Set, 90% to use Test Set.
-        train_set, test_set = featuresets[size:], featuresets[:size]
-        logger.info('Size of feature set: %d, train on %d instances, test on %d instances.' % (len(featuresets), len(train_set), len(test_set)))
-
-        # Train the dialogue act classifier.
-        dialogue_act_classifier = nltk.NaiveBayesClassifier.train(train_set)
-        logger.info('Accuracy: {}%.'.format(round(nltk.classify.accuracy(dialogue_act_classifier, test_set) * 100, 4)))
-
-        # Getting the Precision (% of prediction that are correct), and Recall (% of that identifies the class accurately).
-        refsets = collections.defaultdict(set)
-        testsets = collections.defaultdict(set)
-
-        for i, (features, class_label) in enumerate(test_set):
-            refsets[class_label].add(i)
-            observed = dialogue_act_classifier.classify(features)
-            testsets[observed].add(i)
-
-        for class_label in refsets:
-            precision_rate = precision(refsets[class_label], testsets[class_label])
-            recall_rate = recall(refsets[class_label], testsets[class_label])
-            if not isinstance(precision_rate, float):
-                precision_rate = 0
-            if not isinstance(recall_rate, float):
-                recall_rate = 0
-            logger.info(f'Precision ({class_label}): {round(precision_rate * 100, 4)}%.')
-            logger.info(f'Recall ({class_label}): {round(recall_rate * 100, 4)}%.')
-
+        dialogue_act_classifier = dac_classifier.train()
+        
         with open(trained_dialogue_act_classifier_file, mode="wb") as f:
             pickle.dump(dialogue_act_classifier, f)
             logger.info('Saved trained dialogue act classifier.')
