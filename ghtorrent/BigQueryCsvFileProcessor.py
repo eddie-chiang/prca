d57dca43c00e4f3c6680974ee5372b340cc59187
Eddie Chiang <eddiechiang@Surface-Book.local>
2019-06-09 15:56:10 +1200

    Using Pandas library in order to process the CSV faster.

--- ghtorrent/BigQueryCsvFileProcessor.py
+++ ghtorrent/BigQueryCsvFileProcessor.py
@@ -1,6 +1,7 @@
 import cld2
 import logging
 import math
+import pandas
 from commentexpansion import CommentLoader
 from csv import DictReader, DictWriter
 from dialogueactclassification import Classifier
@@ -39,145 +40,123 @@ class BigQueryCsvFileProcessor:
             return
 
         self.logger.info(f'Start processing {csv_file}...')
+
         dst_csv_file = Path(csv_file.replace('.csv', '_processing.csv'))
-        with open(csv_file, mode='r', encoding='utf-8') as input, open(dst_csv_file, mode='w', encoding='utf-8') as output:
-            dict_reader = DictReader(input, delimiter=',')
 
-            total_rows = sum(1 for row in dict_reader)
-            ctr = 0
-            truncated_ctr = 0
-            non_eng_ctr = 0
-            deleted_ctr = 0
-            skip_ctr = 0
-            progress_pct = 0
+        data_frame = pandas.read_csv(csv_file)
+        total_rows = data_frame.shape[0]
+        self.logger.info(f'Number of rows in {csv_file}: {total_rows}')
 
-            # Seek the file back to the start in order to use dict_reader again.
-            input.seek(0)
-            next(dict_reader)  # Skip header row.
+        ctr = 0
+        truncated_ctr = 0
+        non_eng_ctr = 0
+        deleted_ctr = 0
+        skip_ctr = 0
+        progress_pct = 0
 
-            self.logger.info(f'Number of rows in {csv_file}: {total_rows}')
-            self.logger.info(f'Start generating {dst_csv_file}...')
+        data_frame = pandas.read_csv(csv_file, chunksize=100)
+        for chunk in data_frame:
+            # Add new columns
+            chunk = self.__get_header_fields(chunk)
 
-            writer = DictWriter(output, self.__get_header_fields(
-                dict_reader.fieldnames), delimiter=',')
-            writer.writeheader()
+            chunk[['body', 'is_eng', 'comment_from_mongodb', 'is_deleted']] = chunk.apply(
+                lambda row: self.__load_comment(
+                    row['body'],
+                    row['project_url'],
+                    int(row['pullreq_id']),
+                    int(row['comment_id'])),
+                axis=1)
 
-            for row in dict_reader:
-                row, is_eng, is_truncated, is_deleted = self.__process_row(row)
+            truncated_ctr += len(chunk[chunk['comment_from_mongodb']
+                                       == True].index)
+            non_eng_ctr += len(chunk[chunk['is_eng'] == False].index)
+            deleted_ctr += len(chunk[chunk['is_deleted'] == True].index)
+            skip_ctr += len(chunk[chunk['body'].isnull()].index)
 
-                if row is not None:
-                    writer.writerow(row)
-                else:
-                    skip_ctr += 1
+            # Remove unused columns, and filter rows.
+            chunk.drop(columns='is_eng', inplace=True)
+            chunk.drop(columns='is_deleted', inplace=True)
+            chunk = chunk[chunk['body'].notnull()]
 
-                if is_truncated:
-                    truncated_ctr += 1
+            chunk[['dialogue_act_classification_ml']] = chunk.apply(
+                lambda row: self.dac_classifier.classify(row['body']),
+                axis=1)
 
-                if is_eng is not True:
-                    non_eng_ctr += 1
+            chunk.to_csv(dst_csv_file,
+                         index=False,
+                         header=False if ctr > 0 else True,
+                         mode='w' if ctr == 0 else 'a')
 
-                if is_deleted:
-                    deleted_ctr += 1
-
-                ctr += 1
-
-                progress_pct_floor = math.floor(ctr / total_rows * 10000)
-                if progress_pct_floor != progress_pct:
-                    progress_pct = progress_pct_floor
-                    self.logger.info(
-                        f'Progress: {progress_pct / 100}%, row processed: {ctr}, comment truncated: {truncated_ctr}, deleted: {deleted_ctr}, non English: {non_eng_ctr}, skipped: {skip_ctr}')
+            ctr += chunk.shape[0]
+            # Progress precision: 0.01%.
+            progress_pct_floor = math.floor(ctr / total_rows * 10000)
+            if progress_pct_floor != progress_pct:
+                progress_pct = progress_pct_floor
+                self.logger.info(
+                    f'Progress: {progress_pct / 100}%, row processed: {ctr}, comment truncated: {truncated_ctr}, deleted: {deleted_ctr}, non English: {non_eng_ctr}, skipped: {skip_ctr}')
 
         dst_csv_file.rename(final_file)
         self.logger.info(f'Processing completed, output file: {final_file}')
 
-    def __get_header_fields(self, field_names: list):
-        field_names = field_names + ['comment_from_mongodb'] + ['pr_url'] + ['dialogue_act_classification_ml'] + [
-            'dialogue_act_classification_manual'] + ['dialogue_act_classification_manual_flag']
-
+    def __get_header_fields(self, df: pandas.DataFrame):
         # Remove unused columns.
-        del field_names[field_names.index('description')]
-        del field_names[field_names.index('latest_commit_date')]
-        del field_names[field_names.index('medium_term_commit_count')]
-        del field_names[field_names.index('medium_term_distinct_author_count')]
-        del field_names[field_names.index(
-            'medium_term_distinct_committer_count')]
-        del field_names[field_names.index('recent_commit_count')]
-        del field_names[field_names.index('recent_distinct_author_count')]
-        del field_names[field_names.index('recent_distinct_committer_count')]
-        del field_names[field_names.index('latest_pull_request_history_date')]
-        del field_names[field_names.index('medium_term_pull_request_count')]
-        del field_names[field_names.index('recent_pull_request_count')]
-        del field_names[field_names.index('project_language')]
-        del field_names[field_names.index('project_language_details_language')]
-        del field_names[field_names.index('project_language_bytes')]
-        del field_names[field_names.index('language_percentage')]
-        del field_names[field_names.index('project_language_created_at')]
-        del field_names[field_names.index('forked_from')]
-        del field_names[field_names.index('intra_branch')]
+        df.drop(columns='description', inplace=True)
+        df.drop(columns='latest_commit_date', inplace=True)
+        df.drop(columns='medium_term_commit_count', inplace=True)
+        df.drop(columns='medium_term_distinct_author_count', inplace=True)
+        df.drop(columns='medium_term_distinct_committer_count', inplace=True)
+        df.drop(columns='recent_commit_count', inplace=True)
+        df.drop(columns='recent_distinct_author_count', inplace=True)
+        df.drop(columns='recent_distinct_committer_count', inplace=True)
+        df.drop(columns='latest_pull_request_history_date', inplace=True)
+        df.drop(columns='medium_term_pull_request_count', inplace=True)
+        df.drop(columns='recent_pull_request_count', inplace=True)
+        df.drop(columns='project_language', inplace=True)
+        df.drop(columns='project_language_details_language', inplace=True)
+        df.drop(columns='project_language_bytes', inplace=True)
+        df.drop(columns='language_percentage', inplace=True)
+        df.drop(columns='project_language_created_at', inplace=True)
+        df.drop(columns='forked_from', inplace=True)
+        df.drop(columns='intra_branch', inplace=True)
 
-        return field_names
+        # Add new columns.
+        df['comment_from_mongodb'] = ""
+        df['pr_url'] = ""
+        df['dialogue_act_classification_ml'] = ""
+        df['dialogue_act_classification_manual_flag'] = ""
+        df['dialogue_act_classification_manual'] = ""
 
-    def __process_row(self, row):
+        return df
+
+    def __load_comment(self, comment: str, project_url: str, pullreq_id: int, comment_id: int):
         is_truncated = False
         is_eng = True
         is_deleted = False
 
-        # Remove unused columns.
-        del row['description']
-        del row['latest_commit_date']
-        del row['medium_term_commit_count']
-        del row['medium_term_distinct_author_count']
-        del row['medium_term_distinct_committer_count']
-        del row['recent_commit_count']
-        del row['recent_distinct_author_count']
-        del row['recent_distinct_committer_count']
-        del row['latest_pull_request_history_date']
-        del row['medium_term_pull_request_count']
-        del row['recent_pull_request_count']
-        del row['project_language']
-        del row['project_language_details_language']
-        del row['project_language_bytes']
-        del row['language_percentage']
-        del row['project_language_created_at']
-        del row['forked_from']
-        del row['intra_branch']
-
-        if self.__is_english(row['body']) is not True:
+        if self.__is_english(comment) is not True:
             # Comment detected as not in English, skip the row for further processing.
-            return None, False, is_truncated, is_deleted
-
-        if len(row['body']) == 255:
+            is_eng = False
+            comment = None
+        elif len(comment) == 255:
             # Likely to be a truncated comment, load using CommentLoader.
             is_truncated = True
-            row['comment_from_mongodb'] = True
-            owner = row['project_url'].replace(
-                'https://api.github.com/repos/', '')
+            owner = project_url.replace('https://api.github.com/repos/', '')
             owner = owner[0:owner.index('/')]
-            repo = row['project_url'][row['project_url'].rfind('/') + 1:]
-            pullreq_id = int(row['pullreq_id'])
-            comment_id = int(row['comment_id'])
+            repo = project_url[project_url.rfind('/') + 1:]
 
-            try:              
-                loaded_comment = self.comment_loader.load(
+            try:
+                comment = self.comment_loader.load(
                     owner, repo, pullreq_id, comment_id)
 
-                if loaded_comment is not None:
-                    row['body'] = loaded_comment
-                else:
+                if comment is None:
                     # Comment may have been deleted from GitHub, skip the row for further processing.
                     is_deleted = True
-                    return None, is_eng, is_truncated, is_deleted
             except Exception as e:
                 playsound(self.error_alert_sound, False)
                 self.logger.error(
                     f'Failed to load comment, owner: {owner}, repo: {repo}, pullreq_id: {pullreq_id}, comment_id: {comment_id}, error: {e}')
-                # Return the row without dialogue act classification.
-                return row, is_eng, is_truncated, is_deleted
 
-        row['dialogue_act_classification_ml'] = self.dac_classifier.classify(
-            row['body'])
-
-        return row, is_eng, is_truncated, is_deleted
+        return pandas.Series([comment, is_eng, is_truncated, is_deleted])
 
     def __is_english(self, comment):
         is_reliable, _, details = cld2.detect(comment)
