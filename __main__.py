5ee00c737a7059b481e4db63e79c325648fb96c7
Eddie Chiang <echiang@fraedom.com>
2019-07-01 14:22:14 +1200

    Comment out unused code for now.

--- __main__.py
+++ __main__.py
@@ -72,58 +72,58 @@ def main():
     manual_labelling_file_generator = FileGenerator()
     manual_labelling_file_generator.generate(pull_request_comments_csv_file_processed)
 
-    sys.exit()
+    # sys.exit()
 
-    # Use the model to classify unlabeled data (BigQuery results from the CSV file).
-    comments = collections.defaultdict(set)
-    with open(pull_request_comments_csv_file, mode='r', encoding='utf-8') as input_csvfile:
-        dict_reader = csv.DictReader(input_csvfile, delimiter=',')
+    # # Use the model to classify unlabeled data (BigQuery results from the CSV file).
+    # comments = collections.defaultdict(set)
+    # with open(pull_request_comments_csv_file, mode='r', encoding='utf-8') as input_csvfile:
+    #     dict_reader = csv.DictReader(input_csvfile, delimiter=',')
 
-        # Seek the file back to the start in order to use dict_reader again.
-        input_csvfile.seek(0)
-        next(dict_reader)  # Skip header row.
+    #     # Seek the file back to the start in order to use dict_reader again.
+    #     input_csvfile.seek(0)
+    #     next(dict_reader)  # Skip header row.
 
-        for row in dict_reader:
-            comments[row['comment_id']] = row['body']
+    #     for row in dict_reader:
+    #         comments[row['comment_id']] = row['body']
 
-    # Topic Modelling with Latent Dirichlet Allocation.
-    # Step 1. Tokenization: Split the text into sentences and the sentences into workds. Lowercase the words and remove punctuation.
-    # Step 2. Words that have fewer than 3 characters are removed.
-    # Step 3. All stopwords are removed.
-    # Step 4. Words are lemmatized - words in third person are changed to first person and verbs in past and future tenses are changed into present.
-    # Step 5. Words are stemmed - words are reduced to their root form.
+    # # Topic Modelling with Latent Dirichlet Allocation.
+    # # Step 1. Tokenization: Split the text into sentences and the sentences into workds. Lowercase the words and remove punctuation.
+    # # Step 2. Words that have fewer than 3 characters are removed.
+    # # Step 3. All stopwords are removed.
+    # # Step 4. Words are lemmatized - words in third person are changed to first person and verbs in past and future tenses are changed into present.
+    # # Step 5. Words are stemmed - words are reduced to their root form.
 
-    # Bag of words (BoW)
-    processed_comments = []
-    for comment_id, comment in comments.items():
-        processed_comments.append(topic_modelling_preprocess(comment))
-    bow_dict = gensim.corpora.Dictionary(processed_comments)
+    # # Bag of words (BoW)
+    # processed_comments = []
+    # for comment_id, comment in comments.items():
+    #     processed_comments.append(topic_modelling_preprocess(comment))
+    # bow_dict = gensim.corpora.Dictionary(processed_comments)
 
-    # Filter out tokens that appear in less than 15 documents, or more than 0.5 documents (fraction of total corpus size, not absolute number),
-    # and then only keep the first 100,000 most frequent tokens.
-    bow_dict.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
+    # # Filter out tokens that appear in less than 15 documents, or more than 0.5 documents (fraction of total corpus size, not absolute number),
+    # # and then only keep the first 100,000 most frequent tokens.
+    # bow_dict.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
 
-    # For each document to report how many words and how many times those words appear.
-    bow_corpus = [bow_dict.doc2bow(doc) for doc in processed_comments]
+    # # For each document to report how many words and how many times those words appear.
+    # bow_corpus = [bow_dict.doc2bow(doc) for doc in processed_comments]
 
-    # # Use Term Frequency-Inverse Document Frequency (TD-IDEF) to measure topic relevance.
-    # tfidf = gensim.models.TfidfModel()
+    # # # Use Term Frequency-Inverse Document Frequency (TD-IDEF) to measure topic relevance.
+    # # tfidf = gensim.models.TfidfModel()
 
-    logger.info('Program ended.')
-
-
-def lemmatize_stemming(text):
-    stemmer = SnowballStemmer('english')
-    return stemmer.stem(WordNetLemmatizer().lemmatize(text, pos='v'))
+    # logger.info('Program ended.')
 
 
-def topic_modelling_preprocess(text):
-    result = []
-    for token in gensim.utils.simple_preprocess(text):
-        # All stopwords are removed, and words that have fewer than 3 characters are removed.
-        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
-            result.append(lemmatize_stemming(token))
-    return result
+# def lemmatize_stemming(text):
+#     stemmer = SnowballStemmer('english')
+#     return stemmer.stem(WordNetLemmatizer().lemmatize(text, pos='v'))
+
+
+# def topic_modelling_preprocess(text):
+#     result = []
+#     for token in gensim.utils.simple_preprocess(text):
+#         # All stopwords are removed, and words that have fewer than 3 characters are removed.
+#         if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
+#             result.append(lemmatize_stemming(token))
+#     return result
 
 
 # Execute only if run as a script
